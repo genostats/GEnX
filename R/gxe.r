b57e6e34af171e70292cceb84f6496a2783bcdb8
claire-dandine <claire.dandine@gmail.com>
2018-02-20 13:50:34 +0100

    first commit

---
+++ R/gxe.r
@@ -0,0 +1,143 @@
+genexE.association.test <- function(x, Y = x@ped$pheno, X = matrix(1, nrow(x)), E,
+                             method = c("lm", "lmm"), response = c("quantitative", "binary"), 
+                             test = c("score", "wald", "lrt"), 
+                             K, eigenK, beg = 1, end = ncol(x), p = 0, 
+                             tol = .Machine$double.eps^0.25, ...) {
+
+  if(beg < 1 || end > ncol(x)) stop("range too wide")
+  if(is.null(x@mu)) stop("Need mu to be set in x")
+  if(length(Y) != nrow(x)) stop("Dimensions of Y and x mismatch")
+  if(length(E) != nrow(x)) stop("Dimensions of E and x mismatch")
+  
+  X <- as.matrix(X)
+  if(nrow(X) != nrow(x)) stop("Dimensions of Y and x mismatch")
+  X <- checkX(X, mean(Y))
+
+  response <- match.arg(response)
+  test <- match.arg(test)
+  
+  # check dimensions before anything
+  n <- nrow(x)
+  if(!missing(K)) {
+    if(n != nrow(K) | n != ncol(K)) stop("K and x dimensions don't match")
+  }
+  if(!missing(eigenK)) {
+    if(n != nrow(eigenK$vectors) | n != ncol(eigenK$vectors) | n != length(eigenK$values)) 
+      stop("eigenK and x dimensions don't match")
+  }
+
+  # random effect
+  if(match.arg(method) == "lmm") { 
+
+    # if(response == "binary" & test != "score") {
+    #  warning('Binary phenotype and method = "lmm" force test = "score"')
+    #  test <- "score"
+    # }
+
+    #if(test == "score" | response == "binary") {
+    #  if(missing(K)) stop("For a score test and for binary traits, argument K is mandatory")
+    #  # avec le score test on peut gérer les données manquantes dans Y
+    #  if( any(is.na(Y)) ) {
+    #    w <- !is.na(Y)
+    #    X <- as.matrix(X[w,])
+    #    Y <- Y[w]
+    #    if (is.list(K)) K <- lapply(K, function(x) x[w,w]) else K <- K[w,w]
+    #    warning(sum(!w), 'individuals with missing phenotype are ignored.\n')
+    #  } 
+    #} else {
+      if(missing(eigenK)) 
+        stop("For quantitative Wald and LRT tests, argument eigenK is mandatory")
+      if( any(is.na(Y)) ) 
+        stop("Can't handle missing data in Y, please recompute eigenK for the individuals with non-missing phenotype")
+      if( any(is.na(E)) ) 
+        stop("Can't handle missing data in E, please recompute eigenK for the individuals with non-missing phenotype")
+    #}
+
+    if(response == "quantitative") { # score (argument K), wald ou lrt (eigen K) possibles
+      if(test == "score") {
+	  stop("GxE analysis force response = 'quantitative' and test = 'wald'")
+    #    if(p > 0) 
+    #      X <- cbind(X, eigenK$vectors[,seq_len(p)])
+    #    model <- lmm.aireml(Y, X = X, K, get.P = TRUE, ... )
+    #    t <- .Call("gg_GWAS_lmm_score_f", PACKAGE = "gaston", x@bed, model$Py, model$P, x@mu, beg-1, end-1)
+    #    t$p <- pchisq( t$score, df = 1, lower.tail=FALSE)
+      } else if(test == "wald") {
+        X <- cbind(X, E, 0, 0) # space for the SNP, E and SNPxE
+        t <- .Call("gg_GxE_lmm_wald", PACKAGE = "gaston.env", x@bed, x@mu, Y, X, p, eigenK$values, eigenK$vectors, beg-1, end-1, tol)
+        t$p_3df <- pchisq( t$Wald_3df, df = 3, lower.tail=FALSE)
+		t <- t[c(1:(length(t)-2),length(t),length(t)-1)] # ordre des résultats
+		t$p_2df <- pchisq( t$Wald_2df, df = 2, lower.tail=FALSE)
+		t$Wald_1df <- (t$beta_ExSNP/t$sd_ExSNP)**2
+		t$p_1df <- pchisq( t$Wald_1df, df = 1, lower.tail=FALSE)
+      } else { # test == "lrt"
+	  stop("GxE analysis force response = 'quantitative' and test = 'wald'")
+    #    X <- cbind(X, 0) # space for the SNP
+    #    t <- .Call("gg_GWAS_lmm_lrt", PACKAGE = "gaston", x@bed, x@mu, Y, X, p, eigenK$values, eigenK$vectors, beg-1, end-1, tol)
+    #    t$p <- pchisq( t$LRT, df = 1, lower.tail=FALSE)
+      }
+    } else { # response == "binary", seulement le score test, avec argument K
+	  stop("GxE analysis force response = 'quantitative' and test = 'wald'")
+    #  if(p > 0) 
+    #    X <- cbind(X, eigenK$vectors[,seq_len(p)])
+    #  if(test == "score") {
+    #    model <- logistic.mm.aireml(Y, X = X, K, get.P = TRUE, ... )
+    #    omega <- model$BLUP_omega
+    #    if (!is.null(X)) omega <- omega + X%*%model$BLUP_beta
+    #    pi <- 1/(1+exp(-omega))
+    #    t <- .Call("gg_GWAS_lmm_score_f", PACKAGE = "gaston", x@bed, Y-pi, model$P, x@mu, beg-1, end-1)
+    #    t$p <- pchisq( t$score, df = 1, lower.tail=FALSE) 
+    #  } else if(test == "wald") {
+    #    X <- cbind(X, 0) # space for the SNP
+    #    t <- .Call("gg_GWAS_logitmm_wald_f", PACKAGE = "gaston", x@bed, x@mu, Y, X, K, beg-1, end-1, tol)
+    #    t$p <- pchisq( (t$beta/t$sd)**2, df = 1, lower.tail=FALSE)
+    #  } else stop("LRT test for binary trait not available")
+    }
+  }
+
+  # only fixed effects
+  if(match.arg(method) == "lm") {
+    if(test != "wald") warning('Method = "lm" force test = "wald"')
+    if(response == "quantitative") {
+      if( any(is.na(Y)) ) 
+        stop("Can't handle missing data in Y, please recompute eigenK for the individuals with non-missing phenotype")
+      if(p > 0) X <- cbind(eigenK$vectors[,seq_len(p)], X)
+	  I <- cbind(E, 0, 0)
+      t <- .Call("gg_GxE_lm_quanti", PACKAGE = "gaston.env", x@bed, x@mu, Y, X, I, beg-1, end-1);
+      t$p_3df <- pchisq( t$Wald_3df, df = 3, lower.tail=FALSE)
+      t <- t[c(1:(length(t)-2),length(t),length(t)-1)] # ordre des résultats
+	  t$p_2df <- pchisq( t$Wald_2df, df = 2, lower.tail=FALSE)
+	  t$Wald_1df <- (t$beta_ExSNP/t$sd_ExSNP)**2
+	  t$p_1df <- pchisq( t$Wald_1df, df = 1, lower.tail=FALSE)
+    }
+    if(response == "binary") {
+	  stop("GxE analysis force response = 'quantitative' and test = 'wald'")
+    #  if( any(is.na(Y)) ) 
+    #    stop("Can't handle missing data in Y, please recompute eigenK for the individuals with non-missing phenotype")
+    #  if(p > 0) 
+    #    X <- cbind(X, eigenK$vectors[,seq_len(p)])
+    #  X <- cbind(X,0)
+    #  t <- .Call("gg_GWAS_logit_wald_f", PACKAGE = "gaston", x@bed, x@mu, Y, X, beg-1, end-1, tol);
+    #  t$p <- pchisq( (t$beta/t$sd)**2, df = 1, lower.tail=FALSE)
+    }
+  }
+  L <- list(chr = x@snps$chr, pos = x@snps$pos, id  = x@snps$id)
+  if(beg > 1 | end < ncol(x))  # avoid copy
+    L <- L[beg:end,] 
+
+  data.frame( c( L, t) )
+}
+
+
+checkX <- function(X, mean.y) {
+  X1 <- cbind(1,X)
+  n <- ncol(X1)
+  a <- crossprod(X1)
+  b <- a[ 2:n, 2:n, drop = FALSE ]
+  if( abs(det(b)) < 1e-4 ) stop("Covariate matrix is (quasi) singular")
+  if( abs(det(a)) > 1e-4 & mean.y > 1e-4) {
+    warning("An intercept column was added to the covariate matrix X")
+    return(X1)
+  }
+  return(X)
+}
+
---
+++ src/gxe_lm_quanti.cpp
@@ -0,0 +1,173 @@
+#include <Rcpp.h>
+#include <RcppEigen.h>
+#include "matrix4.h"
+#include "matrix-varia.h"
+#include "wald.h"
+#include <ctime>
+#include <cmath>
+
+#define scalar double
+#define MATRIX MatrixXd
+#define VECTOR VectorXd
+
+using namespace Rcpp;
+using namespace Eigen;
+
+//[[Rcpp::export]]
+List GxE_lm_quanti(XPtr<matrix4> pA, NumericVector mu, NumericVector Y, 
+                   NumericMatrix X, NumericMatrix I_, int beg, int end) {
+
+  int n = Y.size();
+  int r = X.ncol();
+
+  // recopiage des matrices... en float
+  MATRIX y(n,1);
+  MATRIX x(n,r);
+  MATRIX I(n,3);
+  for(int i = 0; i < n; i++) y(i,0) = Y[i];
+
+  for(int i = 0; i < n; i++) 
+    for(int j = 0; j < r; j++)
+      x(i,j) = X(i,j);
+  
+  for(int i = 0; i < n; i++) 
+    for(int j = 0; j < 3; j++)
+      I(i,j) = I_(i,j);
+
+  // Matrix used in estimation computations
+  MATRIX xtx(r,r);
+  MATRIX xtx_i(r,r); 
+  MATRIX xty(r,1);
+  MATRIX xtx_ixty(r,1);
+  MATRIX xtI(r,3);
+  MATRIX xtx_ixtI(r,3);
+  MATRIX Ity(3,1);
+  MATRIX W(3,3);
+  MATRIX W_i(3,3);
+  MATRIX VAR(3,3);
+
+  scalar d, log_d;
+  
+  // Fixed matrix
+  xtx = x.transpose() * x;
+  xty = x.transpose() * y;
+  sym_inverse(xtx, xtx_i, log_d, d, 1e-5);
+  xtx_ixty = xtx_i * xty;
+
+  // declare vectors containing result
+  VECTOR beta(r);
+  VECTOR gamma(3);
+  scalar s2, wald;
+  
+  NumericVector BETA_E(end-beg+1);
+  NumericVector BETA_SNP(end-beg+1);
+  NumericVector BETA_ExSNP(end-beg+1);
+  NumericVector SDBETA_E(end-beg+1);
+  NumericVector SDBETA_SNP(end-beg+1);
+  NumericVector SDBETA_ExSNP(end-beg+1);
+  NumericVector COVBETA_E_SNP(end-beg+1);
+  NumericVector COVBETA_E_ExSNP(end-beg+1);
+  NumericVector COVBETA_SNP_ExSNP(end-beg+1);
+  NumericVector WALD_3df(end-beg+1);
+  NumericVector WALD_2df(end-beg+1);
+  
+  for(int i = beg; i <= end; i++) {
+    VECTOR SNP(n);
+    if( std::isnan(mu(i)) || mu(i) == 0 || mu(i) == 2 ) {
+      BETA_E(i-beg) = NAN;
+      BETA_SNP(i-beg) = NAN;
+      BETA_ExSNP(i-beg) = NAN;
+      SDBETA_E(i-beg) = NAN;
+      SDBETA_SNP(i-beg) = NAN;
+      SDBETA_ExSNP(i-beg) = NAN;
+      COVBETA_E_SNP(i-beg) = NAN;
+      COVBETA_E_ExSNP(i-beg) = NAN;
+      COVBETA_SNP_ExSNP(i-beg) = NAN;
+	  WALD_3df(i-beg) = NAN;
+	  WALD_2df(i-beg) = NAN;
+      continue;
+    }
+
+    // remplir SNP
+    for(int ii = 0; ii < pA->true_ncol-1; ii++) {
+      uint8_t xx = pA->data[i][ii];
+      for(int ss = 0; ss < 4; ss++) {
+        I(4*ii+ss, 1) = ((xx&3) != 3)?(xx&3):mu(i);
+        I(4*ii+ss, 2) = I(4*ii+ss, 0)*I(4*ii+ss, 1);
+        xx >>= 2;
+      }
+    }
+    { int ii = pA->true_ncol-1;
+      uint8_t xx = pA->data[i][ii];
+      for(int ss = 0; ss < 4 && 4*ii+ss < pA->ncol; ss++) {
+        I(4*ii+ss, r-2) = ((xx&3) != 3)?(xx&3):mu(i);
+        I(4*ii+ss, 2) = I(4*ii+ss, 0)*I(4*ii+ss, 1);
+        xx >>= 2;
+      }
+    }
+
+	// Matrix depending on SNP
+	Ity = I.transpose() * y;
+    xtI = x.transpose() * I;
+    xtx_ixtI = xtx_i * xtI;
+    W = I.transpose() * I - xtI.transpose() * ( xtx_i * xtI ); 
+    sym_inverse(W, W_i, log_d, d, 1e-5);
+   
+    beta = xtx_ixty +  xtx_ixtI * ( W_i * ( xtx_ixtI.transpose() * xty ) ) - xtx_ixtI * ( W_i * Ity );
+	gamma = -W_i * ( xtx_ixtI.transpose() * xty ) + W_i * Ity;
+	
+	s2 = (y - x*beta - I*gamma).squaredNorm()/(n-r-3);
+    
+	VAR = s2 * W_i;
+	
+    BETA_E(i-beg) = gamma(0);
+    BETA_SNP(i-beg) = gamma(1);
+    BETA_ExSNP(i-beg) = gamma(2);
+    SDBETA_E(i-beg) = sqrt(VAR(0,0));
+    SDBETA_SNP(i-beg) = sqrt(VAR(1,1));
+    SDBETA_ExSNP(i-beg) = sqrt(VAR(2,2));
+    COVBETA_E_SNP(i-beg) = VAR(0,1);
+    COVBETA_E_ExSNP(i-beg) = VAR(0,2);
+    COVBETA_SNP_ExSNP(i-beg) = VAR(1,2);
+	
+	wald_compute(gamma, VAR, wald);
+	WALD_3df(i-beg) = wald;
+	
+	gamma = gamma.tail(2);
+	VAR = VAR.block(1,1,2,2);
+	wald_compute(gamma, VAR, wald);
+	WALD_3df(i-beg) = wald;
+  }
+  
+  List R;
+  R["beta_E"] = BETA_E;
+  R["beta_SNP"] = BETA_SNP;
+  R["beta_ExSNP"] = BETA_ExSNP;
+  R["sd_E"] = SDBETA_E;
+  R["sd_SNP"] = SDBETA_SNP;
+  R["sd_ExSNP"] = SDBETA_ExSNP;
+  R["cov_E_SNP"] =  COVBETA_E_SNP;
+  R["cov_E_ExSNP"] = COVBETA_E_ExSNP;
+  R["cov_SNP_ExSNP"] = COVBETA_SNP_ExSNP;
+  R["Wald_3df"] = WALD_3df;
+  R["Wald_2df"] = WALD_2df;
+  return R;
+}
+
+
+RcppExport SEXP gg_GxE_lm_quanti(SEXP pASEXP, SEXP muSEXP, SEXP YSEXP, SEXP XSEXP, SEXP I_SEXP, SEXP begSEXP, SEXP endSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject __result;
+    Rcpp::RNGScope __rngScope;
+    Rcpp::traits::input_parameter< XPtr<matrix4> >::type pA(pASEXP);
+    Rcpp::traits::input_parameter< NumericVector >::type mu(muSEXP);
+    Rcpp::traits::input_parameter< NumericVector >::type Y(YSEXP);
+    Rcpp::traits::input_parameter< NumericMatrix >::type X(XSEXP);
+    Rcpp::traits::input_parameter< NumericMatrix >::type I_(I_SEXP);
+    Rcpp::traits::input_parameter< int >::type beg(begSEXP);
+    Rcpp::traits::input_parameter< int >::type end(endSEXP);
+    __result = Rcpp::wrap(GxE_lm_quanti(pA, mu, Y, X, I_, beg, end));
+    return __result;
+END_RCPP
+}
+
